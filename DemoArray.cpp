2fd660fb9b0bd59768f2cdf0d4f408d11baf24cc
Kyogami <ricardoquisp@outlook.com>
2026-01-29 13:02:41 -0500

    Normalize line endings (LF)

--- DemoArray.cpp
+++ DemoArray.cpp
@@ -45,7 +45,7 @@ void DemoArray() {
 
     cout << "Antes del Foreach externo, getSize()=" << arr1.getSize() << endl;
     Foreach(arr1, &IncBasico<T1>);
-    cout << "Terminó" << endl;
+    cout << "Termino" << endl;
 
     using T2 = string;
     CArray< Trait1<T2> > arr2(N);
